51c5162705dc4b64f75695e523c4733848bb1516
sufei <sufei@enaea.edu.cn>
2020-04-28 13:21:38 +0800

    kotlin demo

--- app/src/main/java/com/acorn/testanything/kotlin/KotlinBasic.kt
+++ app/src/main/java/com/acorn/testanything/kotlin/KotlinBasic.kt
@@ -3,6 +3,12 @@ package com.acorn.testanything.kotlin
 /**
  * Created by acorn on 2020/4/28.
  */
+
+fun main() {
+    var test = KotlinBasic()
+    test.inlineFun { "haha" }
+}
+
 class KotlinBasic {
     var what: Int? = 0
         get() {
@@ -16,4 +22,14 @@ class KotlinBasic {
         set(value) {
             field = if (value > 0) 3 else value
         }
+
+    inline fun inlineFun(getString: () -> String?) {
+        //err!!,inline函数的参数不能传给其他非inline方法
+//        otherFunWantFun(getString)
+        println(getString())
+    }
+
+    fun otherFunWantFun(getStr: () -> String?) {
+        println("?${getStr()}")
+    }
 }
