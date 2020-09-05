111259b9c9adaebeeca6809ae56c732a193c5652
oneAcorn <oneAcorn@163.com>
2020-09-05 14:56:29 +0800

    test coroutines

--- app/src/main/java/com/acorn/testanything/kotlin/TestCoroutines.kt
+++ app/src/main/java/com/acorn/testanything/kotlin/TestCoroutines.kt
@@ -1,14 +1,17 @@
 package com.acorn.testanything.kotlin
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 /**
  * Created by acorn on 2020/4/28.
  */
 fun main() {
+//    test1()
+    test2()
+}
+
+private fun test1() {
     val job = GlobalScope.launch { //在后台启用新协程并继续
         delay(1000) //非阻塞延迟1秒
         println("World")
@@ -21,5 +24,34 @@ fun main() {
     }
 }
 
-class TestCoroutines {
+/**
+ * 惰性启动的 async
+可选的，async 可以通过将 start 参数设置为 CoroutineStart.LAZY 而变为惰性的。
+在这个模式下，只有结果通过 await 获取的时候协程才会启动，
+或者在 Job 的 start 函数调用的时候。运行下面的示例：
+ */
+private fun test2() = runBlocking {
+    val time = measureTimeMillis {
+        val one = async(start = CoroutineStart.LAZY) { doSomthing1() }
+        val two = async(start = CoroutineStart.LAZY) { doSomthing2() }
+        two.start()
+        one.start()
+        println("start")
+        delay(1000L)
+        println("start calculating")
+        println("The answer is ${one.await() + two.await()}")
+    }
+    println("Total timeMill:$time")
+}
+
+suspend fun doSomthing1(): Int {
+    println("doSomthing1")
+    delay(1000L)
+    return 22
+}
+
+suspend fun doSomthing2(): Int {
+    println("doSomthing2")
+    delay(1000L)
+    return 33
 }
