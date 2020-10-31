caf5d44cfdfbc7f7f761ea05e736885675044ca7
oneAcorn <oneAcorn@163.com>
2020-10-31 17:15:01 +0800

    MMKV

--- app/src/main/java/com/acorn/testanything/mmkv/Caches.kt
+++ app/src/main/java/com/acorn/testanything/mmkv/Caches.kt
@@ -7,6 +7,10 @@ import com.tencent.mmkv.MMKV
  */
 object Caches {
     private val mmkv = MMKV.mmkvWithID("test", MMKV.SINGLE_PROCESS_MODE)
+
     var str1: String? by MMKVDelegate(mmkv, String::class.java)
     var testBean1: TestBean? by MMKVDelegate(mmkv, TestBean::class.java)
+
+    var str2: String by mmkv.string()
+    var testBean2: TestBean by mmkv.parcelable()
 }
--- app/src/main/java/com/acorn/testanything/mmkv/MMKVTestActivity.kt
+++ app/src/main/java/com/acorn/testanything/mmkv/MMKVTestActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.acorn.testanything.R
 import com.acorn.testanything.utils.logI
+import com.tencent.mmkv.MMKV
 import kotlinx.android.synthetic.main.activity_mmkv_test.*
 
 /**
@@ -17,9 +18,11 @@ class MMKVTestActivity : AppCompatActivity() {
         btn1.setOnClickListener {
             Caches.str1 = "abc"
             Caches.testBean1 = TestBean(null, "abc")
+            Caches.str2 = "def"
+            Caches.testBean2 = TestBean("abc", "def")
         }
         readBtn.setOnClickListener {
-            logI("caches ${Caches.str1},${Caches.testBean1}")
+            logI("caches ${Caches.str1},${Caches.testBean1},${Caches.str2},${Caches.testBean2}")
         }
     }
 }
