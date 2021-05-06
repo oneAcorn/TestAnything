a6f5e4560cb9ee1dbf744690907555ca9ce3b768
oneAcorn <oneAcorn@163.com>
2021-05-06 17:30:17 +0800

    MotionLayout

--- app/src/main/java/com/acorn/testanything/motionLayout/MotionLayoutActivity.kt
+++ app/src/main/java/com/acorn/testanything/motionLayout/MotionLayoutActivity.kt
@@ -2,6 +2,7 @@ package com.acorn.testanything.motionLayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.acorn.testanything.R
 
 /**
  * Created by acorn on 2019-08-26.
@@ -9,6 +10,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MotionLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_motionlayout)
+        setContentView(R.layout.activity_motion_layout_scene1)
     }
 }
